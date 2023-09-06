913473e4e8f1dda27ef3c7069bdaed8c10f00bee
Agustin Marquez <agustinmarquez@Agustins-MacBook-Pro.local>
2023-09-06 13:59:06 +0200

    add openai packages to monkey patch

--- nebuly/init.py
+++ nebuly/init.py
@@ -3,7 +3,26 @@ from nebuly.monkey_patcher import (check_no_packages_already_imported,
                                    import_and_patch_packages)
 from nebuly.observer import NebulyObserver
 
-PACKAGES = [Package("openai", ["0.10.2"], ["Completion.create"])]
+PACKAGES = [
+    Package(
+        "openai",
+        ["0.10.2"],
+        [
+            "Completion.create",
+            "Completion.create",
+            "ChatCompletion.create",
+            "Edit.create",
+            "Image.create",
+            "Image.create_edit",
+            "Image.create_variation",
+            "Embedding.create",
+            "Audio.transcribe",
+            "Audio.translate",
+            "FineTune.create",
+            "Moderation.create",
+        ],
+    )
+]
 
 
 def init(*, api_key: str, project: str, phase: str) -> None:
