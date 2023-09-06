288b8d35f2b17a89a714dce0e47c33218412628e
Agustin Marquez <agustinmarquez@Agustins-MacBook-Pro.local>
2023-09-06 13:50:33 +0200

    refactor: join monkey patcher and patcher

--- nebuly/monkey_patcher.py
+++ nebuly/monkey_patcher.py
@@ -1,12 +1,14 @@
-import sys
-import logging
 import importlib
+import logging
+import sys
+from copy import deepcopy
+from datetime import datetime, timezone
+from functools import wraps
 from types import ModuleType
-from nebuly.entities import Package
+from typing import Any
 
+from nebuly.entities import Observer_T, Package, Watched
 from nebuly.exceptions import AlreadyImportedError
-from nebuly.patcher import Observer_T, patcher
-
 
 logger = logging.getLogger(__name__)
 
@@ -51,5 +53,59 @@ def _monkey_patch_attribute(
     setattr(
         tmp_component,
         path[-1],
-        patcher(observer)(getattr(tmp_component, path[-1])),
+        _patcher(observer)(getattr(tmp_component, path[-1])),
     )
+
+
+def _split_nebuly_kwargs(
+    kwargs: dict[str, Any]
+) -> tuple[dict[str, Any], dict[str, Any]]:
+    """
+    Split nebuly kwargs from function kwargs
+    """
+
+    nebuly_kwargs = {}
+    function_kwargs = {}
+    for key in kwargs:
+        if key.startswith("nebuly_"):
+            nebuly_kwargs[key] = kwargs[key]
+        else:
+            function_kwargs[key] = kwargs[key]
+    return nebuly_kwargs, function_kwargs
+
+
+def _patcher(observer: Observer_T):
+    """
+    Decorator that calls observer with a Watched instance when the decorated
+    function is called
+    """
+
+    def inner(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            nebuly_kwargs, function_kwargs = _split_nebuly_kwargs(kwargs)
+
+            original_args = deepcopy(args)
+            nebuly_kwargs = deepcopy(nebuly_kwargs)
+            original_kwargs = deepcopy(function_kwargs)
+
+            called_at = datetime.now(timezone.utc)
+
+            result = f(*args, **function_kwargs)
+
+            original_result = deepcopy(result)
+            watched = Watched(
+                function=f,
+                called_at=called_at,
+                called_with_args=original_args,
+                called_with_kwargs=original_kwargs,
+                called_with_nebuly_kwargs=nebuly_kwargs,
+                returned=original_result,
+            )
+            observer(watched)
+
+            return result
+
+        return wrapper
+
+    return inner
--- tests/test_monkey_patcher.py
+++ tests/test_monkey_patcher.py
@@ -1,12 +1,118 @@
-import pytest
+from datetime import datetime, timezone
 
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+
+from nebuly.entities import Package, Watched
 from nebuly.exceptions import AlreadyImportedError
-from nebuly.entities import Watched, Package
-from nebuly.monkey_patcher import (
-    check_no_packages_already_imported,
-    _monkey_patch,
-    import_and_patch_packages,
+from nebuly.monkey_patcher import (_monkey_patch, _split_nebuly_kwargs,
+                                   check_no_packages_already_imported,
+                                   import_and_patch_packages, _patcher)
+
+any = st.one_of(
+    st.integers(), st.floats(), st.text(), st.booleans(), st.none(), st.binary()
 )
+
+
+class Observer:
+    def __init__(self) -> None:
+        self.watched: list[Watched] = []
+
+    def __call__(self, watched: Watched) -> None:
+        self.watched.append(watched)
+
+
+@given(args=st.tuples(any), kwargs=st.dictionaries(st.text(), any))
+def test_patcher_doesnt_change_any_behavior(args, kwargs) -> None:
+    def to_patched(*args: int, **kwargs: str):
+        """This is the docstring to be tested"""
+        return args, kwargs
+
+    patched = _patcher(lambda _: None)(to_patched)
+
+    assert patched(*args, **kwargs) == to_patched(*args, **kwargs)
+    assert patched.__name__ == to_patched.__name__
+    assert patched.__doc__ == to_patched.__doc__
+    assert patched.__module__ == to_patched.__module__
+    assert patched.__qualname__ == to_patched.__qualname__
+    assert patched.__annotations__ == to_patched.__annotations__
+
+
+@given(args=st.tuples(any), kwargs=st.dictionaries(st.text(), any))
+def test_patcher_calls_observer(args, kwargs) -> None:
+    def to_patched(*args: int, **kwargs: str):
+        """This is the docstring to be tested"""
+        return args, kwargs
+
+    observer = Observer()
+
+    patched = _patcher(observer)(to_patched)
+
+    before = datetime.now(timezone.utc)
+    patched(*args, **kwargs)
+    after = datetime.now(timezone.utc)
+
+    assert len(observer.watched) == 1
+    watched = observer.watched[0]
+    assert watched.function == to_patched
+    assert before <= watched.called_at <= after
+    assert watched.called_with_args == args
+    assert watched.called_with_kwargs == kwargs
+    assert watched.called_with_nebuly_kwargs == {}
+    assert watched.returned == to_patched(*args, **kwargs)
+
+
+def test_watched_is_immutable() -> None:
+    def to_patched(mutable: list[int]) -> list[int]:
+        mutable.append(1)
+        return mutable
+
+    observer = Observer()
+    mutable: list[int] = []
+
+    _patcher(observer)(to_patched)(mutable)
+
+    mutable.append(2)
+
+    assert len(observer.watched) == 1
+    watched = observer.watched[0]
+    assert watched.called_with_args == ([],)
+    assert watched.returned == [1]
+
+
+def test_split_nebuly_kwargs():
+    original_dict = {
+        "nebuly_segment": "segment",
+        "arg1": "arg1",
+        "nebuly_project": "project",
+        "arg2": "arg2",
+    }
+    nebuly_kwargs, function_kwargs = _split_nebuly_kwargs(original_dict)
+    assert nebuly_kwargs == {
+        "nebuly_segment": "segment",
+        "nebuly_project": "project",
+    }
+    assert function_kwargs == {"arg1": "arg1", "arg2": "arg2"}
+
+
+def test_nebuly_args_are_intercepted():
+    def function(a: int, b: int) -> int:
+        return a + b
+
+    observer = Observer()
+    patched = _patcher(observer)(function)
+
+    patched(1, 2, nebuly_segment="segment", nebuly_project="project")
+
+    assert len(observer.watched) == 1
+    watched = observer.watched[0]
+    assert watched.called_with_args == (1, 2)
+    assert watched.called_with_nebuly_kwargs == {
+        "nebuly_segment": "segment",
+        "nebuly_project": "project",
+    }
+    assert watched.returned == 3
 
 
 def test_fails_when_package_already_imported() -> None:
