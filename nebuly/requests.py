b99e2321b9433c5f95b2a124c5135cad9c96e75e
Agustin Marquez <agustinmarquez@Agustins-MacBook-Pro.local>
2023-09-07 12:03:43 +0200

    improve requests

--- nebuly/requests.py
+++ nebuly/requests.py
@@ -1,13 +1,20 @@
 import json
+import logging
 import urllib.request
 
+logger = logging.getLogger(__name__)
 
-def post_json_data(url: str, data_dict: dict[str, str]) -> urllib.request.Request:
-    json_data = json.dumps(data_dict).encode("utf-8")
 
+def post_json_data(url: str, data_dict: dict[str, str]):
+    json_data = json.dumps(data_dict, default=str).encode("utf-8")
+
+    logger.debug("json_data: %s", json_data)
     request = urllib.request.Request(
         url, data=json_data, headers={"Content-Type": "application/json"}
     )
 
     with urllib.request.urlopen(request, timeout=3) as response:
-        return response.readlines()
+        response_body = response.read().decode("utf-8")
+        logger.debug("response_body: %s", response_body)
+
+    return response_body
