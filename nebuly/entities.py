30c539a82bb6114557f65fc0f7b8344364f6f635
Agustin Marquez <agustinmarquez@Agustins-MacBook-Pro.local>
2023-09-07 17:07:47 +0200

    add enums from main

--- nebuly/entities.py
+++ nebuly/entities.py
@@ -1,6 +1,39 @@
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
 from typing import Any, Callable
+
+
+class Task(Enum):
+    UNKNOWN = "undetected"  # XXX: should be "unknown"?
+    TEXT_GENERATION = "text_generation"
+    TEXT_SUMMARIZATION = "text_summarization"
+    TEXT_CLASSIFICATION = "text_classification"
+    TEXT_EMBEDDING = "text_embedding"
+    TEXT_EDITING = "text_editing"
+    TEXT_MODERATION = "text_moderation"
+    CHAT = "chat"
+    IMAGE_GENERATION = "image_generation"
+    IMAGE_EDITING = "image_editing"
+    IMAGE_VARIATION = "image_variation"
+    AUDIO_TRANSCRIPTION = "audio_transcription"
+    AUDIO_TRANSLATION = "audio_translation"
+    FINETUNING = "fine-tuning"
+
+
+class DevelopmentPhase(Enum):
+    EXPERIMENTATION = "experimentation"
+    PREPROCESSING = "preprocessing"
+    TRAINING = "training"
+    FINETUNING = "fine-tuning"
+    PRODUCTION = "production"
+    UNKNOWN = "unknown"
+
+
+class Provider(Enum):
+    UNKNOWN = "unknown"
+    AZURE_OPENAI = "azure_openai"
+    OPENAI = "openai"
 
 
 @dataclass(frozen=True, slots=True)
