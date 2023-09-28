f3b04c72281b1048249d0390855dae63601d298c
Valerio Sofi <v.sofi@nebuly.ai>
2023-09-28 17:03:32 +0200

    chore: Add test

--- tests/test_langchain.py
+++ tests/test_langchain.py
@@ -368,3 +368,59 @@ def test_langchain__chain_with_function_tool(
                 json.dumps(interaction_watch.to_dict(), cls=CustomJSONEncoder)
                 is not None
             )
+
+
+def test_langchain_sequential_chain(openai_completion):
+    with patch("openai.Completion.create") as mock_completion_create:
+        with patch.object(NebulyObserver, "on_event_received") as mock_observer:
+            mock_completion_create.return_value = openai_completion
+            nebuly_init(mock_observer)
+            llm = OpenAI(temperature=0.7)
+            synopsis_template = """You are a playwright. Given the title of play and
+            the era it is set in, it is your job to write a synopsis for that title.
+
+            Title: {title}
+            Era: {era}
+            Playwright: This is a synopsis for the above play:"""
+            synopsis_prompt_template = PromptTemplate(
+                input_variables=["title", "era"], template=synopsis_template
+            )
+            synopsis_chain = LLMChain(
+                llm=llm, prompt=synopsis_prompt_template, output_key="synopsis"
+            )
+
+            # This is an LLMChain to write a review of a play given a synopsis.
+            llm = OpenAI(temperature=0.7)
+            template = """You are a play critic from the New York Times. Given the
+            synopsis of play, it is your job to write a review for that play.
+
+            Play Synopsis:
+            {synopsis}
+            Review from a New York Times play critic of the above play:"""
+            prompt_template = PromptTemplate(
+                input_variables=["synopsis"], template=template
+            )
+            review_chain = LLMChain(
+                llm=llm, prompt=prompt_template, output_key="review"
+            )
+
+            # This is the overall chain where we run these two chains in sequence.
+            from langchain.chains import SequentialChain
+
+            overall_chain = SequentialChain(
+                chains=[synopsis_chain, review_chain],
+                input_variables=["era", "title"],
+                # Here we return multiple variables
+                output_variables=["synopsis", "review"],
+            )
+
+            overall_chain(
+                {"title": "Tragedy at sunset on the beach", "era": "Victorian England"},
+            )
+
+            assert mock_observer.call_count == 1
+            interaction_watch = mock_observer.call_args[0][0]
+            assert isinstance(interaction_watch, InteractionWatch)
+            assert "You are a playwright" in interaction_watch.input
+            assert "Victorian England" in interaction_watch.input
+            assert isinstance(interaction_watch.output.get("review"), str)
