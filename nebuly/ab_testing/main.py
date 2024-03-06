428f93d8aa2c6784e817a379b653196e153094fd
diegofiori <d.fiori@nebuly.ai>
2024-03-06 12:19:55 +0100

    add feedback actions tracking

--- nebuly/ab_testing/main.py
+++ nebuly/ab_testing/main.py
@@ -1,3 +1,4 @@
+# pylint: disable=duplicate-code
 import asyncio
 import json
 import os
---
+++ nebuly/tracking/types.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import Any, Optional
+
+
+@dataclass
+class FeedbackActionMetadata:
+    input: Optional[str]
+    output: Optional[str]
+    end_user: str
+    timestamp: datetime
+    anonymize: bool
+    end_user_group_profile: Optional[str] = None
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "input": self.input,
+            "output": self.output,
+            "end_user": self.end_user,
+            "end_user_group_profile": self.end_user_group_profile,
+            "timestamp": self.timestamp,
+            "anonymize": self.anonymize,
+        }
+
+
+class FeedbackActionName(str, Enum):
+    THUMBS_UP = "thumbs_up"
+    THUMBS_DOWN = "thumbs_down"
+    COPY_INPUT = "copy_input"
+    COPY_OUTPUT = "copy_output"
+    PASTE = "paste"
+
+
+@dataclass
+class FeedbackAction:
+    slug: FeedbackActionName
+    extras: Optional[dict[str, Any]] = None
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "slug": self.slug,
+            **(self.extras or {}),
+        }
